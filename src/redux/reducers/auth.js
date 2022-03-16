a4f2f6a9316848dd54dff93f9a65a51e68bfc89d
akimajid <akimajid07@gmail.com>
2022-03-16 09:52:53 +0700

    setup redux

---
+++ src/redux/reducers/auth.js
@@ -0,0 +1,11 @@
+const init_state = {
+    id: 0,
+    username: "",
+    email: "",
+    fullname: ""
+}
+
+export const auth_reducer = (state = init_state, action) => {
+    return state
+}
+
---
+++ src/redux/store.js
@@ -0,0 +1,7 @@
+import { auth_reducer } from "./reducers";
+import { combineReducers } from "redux"
+
+
+export default combineReducers({
+    auth: auth_reducer
+})
