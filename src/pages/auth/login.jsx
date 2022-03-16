396610a36af79d827c21d595c0f4bc0df14eddce
akimajid <akimajid07@gmail.com>
2022-03-16 10:29:06 +0700

    login page

---
+++ src/pages/auth/login.jsx
@@ -0,0 +1,71 @@
+import {
+    Box,
+    Container,
+    FormLabel,
+    Heading,
+    Input,
+    InputGroup,
+    InputRightElement,
+    Stack,
+    Text,
+    Icon,
+    Button,
+  } from "@chakra-ui/react";
+  import jsCookie from "js-cookie";
+  import api from "../../lib/api";
+  import { IoMdEye, IoMdEyeOff } from "react-icons/io";
+  import { useState } from "react";
+  import { useFormik } from "formik"
+  
+  const LoginPage = () => {
+    const [passwordVisible, setPasswordVisible] = useState(false);
+
+    const formik = useFormik({
+        initialValues: {
+            username: "",
+            password: ""
+        }
+    })
+  
+    return (
+      <Container maxW="lg">
+        <Stack py="10" spacing={12}>
+          <Heading textAlign="center">Sign in to start shopping</Heading>
+          <Box maxW="lg" backgroundColor="white" shadow="xl" p="8">
+            <form>
+              <FormLabel htmlFor="inputUsername">Username</FormLabel>
+              <Input id="inputUsername" />
+  
+              <FormLabel mt="4" htmlFor="inputPassword">
+                Password
+              </FormLabel>
+              <InputGroup>
+                <Input
+                  type={passwordVisible ? "text" : "password"}
+                  id="inputPassword"
+                />
+                <InputRightElement
+                  children={
+                    <Icon
+                      fontSize="xl"
+                      onClick={() => setPasswordVisible(!passwordVisible)}
+                      as={passwordVisible ? IoMdEyeOff : IoMdEye}
+                      sx={{ _hover: { cursor: "pointer" } }}
+                    />
+                  }
+                />
+              </InputGroup>
+  
+              <Stack>
+                <Button mt="4" colorScheme="blue">
+                  Login
+                </Button>
+              </Stack>
+            </form>
+          </Box>
+        </Stack>
+      </Container>
+    );
+  };
+  
+  export default LoginPage;
