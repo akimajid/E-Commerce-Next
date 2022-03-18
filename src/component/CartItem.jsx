7e49a35c1c622d2d965f955acad73083fea160d6
akimajid <akimajid07@gmail.com>
2022-03-18 10:48:25 +0700

    delete cart

--- src/component/CartItem.jsx
+++ src/component/CartItem.jsx
@@ -11,8 +11,94 @@ import {
   Text,
 } from "@chakra-ui/react";
 import { IoMdClose } from "react-icons/io";
+import { useDispatch } from "react-redux";
+import axiosInstance from "../lib/api";
+import { fetchUserCart } from "../redux/actions/cart";
+import { cart_types } from "../redux/types";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import _ from "lodash";
+import { useCallback } from "react";
 
-const CartItem = () => {
+const CartItem = ({
+  imageUrl,
+  productName,
+  category,
+  quantity,
+  price,
+  id,
+  stock,
+  cartIndex,
+}) => {
+  const dispatch = useDispatch();
+
+  const deleteCartItem = async () => {
+    try {
+      await axiosInstance.delete(`/carts/${id}`);
+
+      // Cara pertama: melakukan manipulasi store redux untuk update data "terbaru"
+      dispatch({
+        type: cart_types.DELETE_ITEM,
+        payload: cartIndex,
+      });
+
+      // Cara kedua: fetch ke database lagi untuk update data cart terbaru
+      // dispatch(fetchUserCart())
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const debounceQtyInputHandler = useCallback(
+    _.debounce(async (value) => {
+      await axiosInstance.patch(`/carts/${id}`, {
+        quantity: value,
+      });
+
+      dispatch({
+        type: cart_types.EDIT_QTY,
+        payload: {
+          idx: cartIndex,
+          quantity: value,
+        },
+      });
+    }, 1500),
+    []
+  );
+
+  const qtyInputHandler = (event) => {
+    const { value } = event.target;
+
+    if (value === "") {
+      formik.setFieldValue("quantity", value);
+      return;
+    }
+
+    const parsedValue = parseInt(value);
+
+    if (isNaN(parsedValue)) return;
+
+    if (parsedValue < 0) return;
+
+    if (parsedValue > stock) return;
+
+    try {
+      debounceQtyInputHandler(parsedValue);
+      formik.setFieldValue("quantity", parsedValue);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const formik = useFormik({
+    initialValues: {
+      quantity: quantity,
+    },
+    validationSchema: Yup.object().shape({
+      quantity: Yup.number().required().min(1).max(stock),
+    }),
+  });
+
   return (
     <Grid templateColumns="repeat(7, 1fr)" my={4}>
       <GridItem w="100%" display="flex" alignItems="center">
@@ -20,28 +106,44 @@ const CartItem = () => {
           objectFit="cover"
           boxSize="120px"
           borderRadius={8}
-          src={
-            "https://d29c1z66frfv6c.cloudfront.net/pub/media/catalog/product/large/9999deeeddc8471743761964befbc60e9747d3eb_xxl-1.jpg"
-          }
+          src={imageUrl}
+          fallbackSrc={"https://via.placeholder.com/120"}
         />
       </GridItem>
       <GridItem w="100%" colSpan={2} display="flex" alignItems="center">
         <Flex direction="column">
-          <Text fontWeight="medium">Product Name</Text>
+          <Text fontWeight="medium">{productName || "Product"}</Text>
           <Text color="gray.600" fontSize="sm">
-            Category
+            {category || "Category"}
           </Text>
         </Flex>
       </GridItem>
 
       <GridItem w="100%" display="flex" alignItems="center">
-        <Input boxSize={12} textAlign="center" type="number" />
+        <Input
+          h={12}
+          w={24}
+          textAlign="center"
+          type="number"
+          value={formik.values.quantity}
+          onChange={qtyInputHandler}
+        />
       </GridItem>
-      <GridItem w="100%" colSpan={2} display="flex" alignItems="center">
-        <Text fontWeight="medium">Rp {(120000).toLocaleString()}</Text>
+      <GridItem
+        w="100%"
+        colSpan={2}
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Text fontWeight="medium">Rp {price?.toLocaleString()}</Text>
       </GridItem>
       <GridItem w="100%" display="flex" alignItems="center">
-        <IconButton colorScheme="red" icon={<Icon as={IoMdClose} />} />
+        <IconButton
+          onClick={deleteCartItem}
+          colorScheme="red"
+          icon={<Icon as={IoMdClose} />}
+        />
       </GridItem>
     </Grid>
   );
